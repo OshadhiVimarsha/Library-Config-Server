0fd54fc7e6141d5012a606ac12ed8fe4429808e3
oshivimarsha <oshadivimarsha@gmail.com>
2026-03-30 08:24:48 +0100

    Update Changes

--- src/main/java/lk/ijse/eca/configserver/ConfigServerApplication.java
+++ src/main/java/lk/ijse/eca/configserver/ConfigServerApplication.java
@@ -1,9 +1,11 @@
-package lk.ijse.configserver;
+package lk.ijse.eca.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
